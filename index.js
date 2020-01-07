c8f0d00eae3705089520a56e1c4624097eb313ec
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2020-01-07 22:37:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+document.getElementById("buttonkop").onclick = function(){
+    document.getElementById("buttonkop").innerHTML = "Sorry, het is uitverkocht";
+}
+
