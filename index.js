64e793f86e767c7a7cf7b24df960754ae680887a
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2020-01-16 00:06:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,44 @@
 
 /*eslint 'no-console': 0*/
 
-document.getElementById("buttonkop").onclick = function(){
-    document.getElementById("buttonkop").innerHTML = "Sorry, het is uitverkocht";
+
+var audio, playbtn;
+
+function initAudioPlayer() {
+    audio = new Audio();
+    audio.src = "test.mp3";
+    audio.loop = true;
+    audio.play();
+    // Set object references
+    playbtn = document.getElementById("muziek");
+    // Add Event Handling
+    playbtn.addEventListener("click", playPause);
+    // Functions
+    function playPause() {
+        if (audio.paused) {
+            audio.play();
+            document.getElementById("muziek").innerHTML = "pauze";
+        } else {
+            audio.pause();
+            document.getElementById("muziek").innerHTML = "Verder spelen";
+
+        }
+    }
+
 }
+window.addEventListener("load", initAudioPlayer);
+
+// https://www.developphp.com/video/JavaScript/Audio-Play-Pause-Mute-Buttons-Tutorial //
+
+
+document.getElementById("buttonkop").onclick = function () {
+    document.getElementById("buttonkop").innerHTML = "Sorry, het is uitverkocht";
+};
+
+
+document.getElementById("hartje").onclick = function () {
+       var element = document.getElementById("hartje");
+        element.classList.toggle("rood");
+};
+
 
